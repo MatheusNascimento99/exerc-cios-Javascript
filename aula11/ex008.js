0873122228bfb16c6ba90e89b90f29daa6f2f54f
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-10-14 13:08:57 -0300

    ex 01 - 12

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+velo = 12
+console.log(`A velocidade é de ${velo} km por hora`)
+if (velo > 60){
+    console.log('Você foi MULTADO, velocidade excedida!')
+}
+console.log('Dirija com cuidade, use cinto de segurança!')
---
+++ aula12/ex.011.js
@@ -0,0 +1,9 @@
+var idade = 15
+
+if (idade  <= 15 ){ 
+    console.log ('Não pode votar!')
+} else if (idade <=17) {
+    console.log('Voto opcional!')
+} else {
+    console.log('Voto obrigatório!')
+}
